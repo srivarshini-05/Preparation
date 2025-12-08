aa0153acd53783de331da2865873514c9e5c7745
srivarshini-05 <165925681+srivarshini-05@users.noreply.github.com>
2025-12-08 10:11:49 +0530

    Update 3_houseRob.java

--- DP/DP_BASICS/3_houseRob.java
+++ DP/DP_BASICS/3_houseRob.java
@@ -1,11 +1,22 @@
-class Solution {
-    public int rob(int[] nums) {
-        int n = nums.length;
-        int[] f = new int[n + 1];
-        f[1] = nums[0];
-        for (int i = 2; i <= n; ++i) {
-            f[i] = Math.max(f[i - 1], f[i - 2] + nums[i - 1]);
+package dynamicProgramming;
+import java.util.*;
+public class rob1 {
+    public static int rob(int[] arr){
+        int n=arr.length;
+        //if(n==1){
+         //   return arr[0];
+        //}
+        int[] dp=new int[n];
+        dp[0]=arr[0];
+
+        dp[1]=Math.max(arr[0],arr[1]);
+        for(int i=2;i<n;i++){
+            dp[i]=Math.max(dp[i-1],dp[i-2]+arr[i]);
         }
-        return f[n];
+        return dp[n-1];
     }
+    public static void main(String[] args){
+        int[] arr={2,3,4,8};
+        System.out.print(rob(arr);
+        }
 }
