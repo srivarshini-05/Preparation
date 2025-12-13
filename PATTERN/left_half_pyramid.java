3fcccf83a36649b82f6e1e70c2667e510e4762ec
srivarshini-05 <165925681+srivarshini-05@users.noreply.github.com>
2025-12-13 09:31:40 +0530

    Create left_half_pyramid.java

---
+++ PATTERN/left_half_pyramid.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int k=n-1;k>=i;k--){
+                System.out.print("  ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for(int i=1;i<=n;i++){
+            for(int k=n-1;k>=i;k--){
+                System.out.print("  ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+        for(int i=1;i<=n;i++){
+            for(int k=n-1;k>=i;k--){
+                System.out.print("  ");
+            }
+            for(int j=0;j<i;j++){
+                char a=(char)('A'+j);
+                System.out.print(a+" ");
+            }
+            System.out.println();
+        }
+    }
+}
