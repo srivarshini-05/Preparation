c2bc48f5af2ba6ef6acff867022dc4167988699e
srivarshini-05 <165925681+srivarshini-05@users.noreply.github.com>
2025-12-20 09:42:38 +0530

    Update reverse_II.java

--- LINKED_LIST/LEVEL 2/reverse_II.java
+++ LINKED_LIST/LEVEL 2/reverse_II.java
@@ -32,3 +32,76 @@ class Solution {
         
     }
 }
+
+/////////////////////////////////////////////////////////////////////////////////
+
+//my approch
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode rev(ListNode s,ListNode e,int left,int right){
+        ListNode curr=s;
+        ListNode prev=null;
+        //ListNode nextn=null;
+        for(int i=0;i<right-left+1;i++){
+            ListNode nextn=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=nextn;
+        }
+        //ListNode l=nextn;
+        return prev;
+    }
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        if(head==null || left==right)return head;
+        ListNode s=head;
+        int l=1;
+        while(s!=null){
+            if(left==l)break;
+            s=s.next;
+            l++;
+        }
+        ListNode so=head;
+        int l1=1;
+        while(so!=null){
+            if(left-1==l1)break;
+            so=so.next;
+            l1++;
+        }
+        int r=1;
+        ListNode e=head;
+        while(e!=null){
+            if(right==r)break;
+            e=e.next;
+            r++;
+        }
+        int r1=1;
+        ListNode en=head;
+        while(en!=null){
+            if(right+1==r1)break;
+            en=en.next;
+            r1++;
+        }
+        //s.next=e.next;
+        ListNode prev=rev(s,e,left,right);
+        if(so!=null){
+            so.next=prev;
+        }
+        else{
+            head=prev;
+        }
+    
+        //so.next=e;
+        s.next=en;
+        return head;
+        
+    }
+}
