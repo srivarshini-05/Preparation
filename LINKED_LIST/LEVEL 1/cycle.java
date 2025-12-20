541f929eb3e203e77120bda86b0121033645a2fe
srivarshini-05 <165925681+srivarshini-05@users.noreply.github.com>
2025-12-20 09:25:29 +0530

    Create cycle.java

---
+++ LINKED_LIST/LEVEL 1/cycle.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode s=head;
+        ListNode f=head;
+        while(f!=null && f.next!=null){
+            s=s.next;
+            f=f.next.next;
+            if(s==f){
+                return true;
+            }
+        }
+        return false;
+    }
+}
