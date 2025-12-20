5f0c6ec836732ce011c5c0b4743f7609eb129a1a
srivarshini-05 <165925681+srivarshini-05@users.noreply.github.com>
2025-12-20 09:25:05 +0530

    Update Insert_display.java

--- LINKED_LIST/LEVEL 1/Insert_display.java
+++ LINKED_LIST/LEVEL 1/Insert_display.java
@@ -1,1 +1,44 @@
 
+import java.util.*;
+
+class Node{
+    int data;
+    Node next;
+    Node(int d){
+        data=d;
+        next=null;
+        }
+}
+class ll{
+    Node head=null;
+    Node prev=null;
+    void inst(int d){
+        Node nn=new Node(d);
+        if(head==null){
+            head=nn;
+            prev=nn;
+        }
+        else{
+            prev.next=nn;
+            prev=nn;
+        }
+    }
+    void dis(){
+        Node t=head;
+        while(t!=null){
+            System.out.print(t.data+" ");
+            t=t.next;
+        }
+    }
+
+    }
+public class intro {
+    public static void main(String[] args){
+        int[] arr={1,2,3};
+        ll a=new ll();
+        for(int d:arr){
+            a.inst(d);
+        }
+        a.dis();
+    }
+}
