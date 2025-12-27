366cb5c7c7f8aecc0246be6ef0f13ee486b5f079
srivarshini-05 <165925681+srivarshini-05@users.noreply.github.com>
2025-12-27 12:11:22 +0530

    Create online_stock_span.java

---
+++ STACK/LEVEL 2/online_stock_span.java
@@ -0,0 +1,23 @@
+class StockSpanner {
+    Stack<int[]>s=new Stack<>();
+    int ind;
+    public StockSpanner() {
+        ind=-1;
+    }
+    
+    public int next(int price) {
+        ind+=1;
+        while(!s.isEmpty() && s.peek()[0]<=price){
+            s.pop();
+        }
+        int ans=ind-(s.isEmpty()?-1:s.peek()[1]);
+        s.push(new int[]{price,ind});
+        return ans;
+    }
+}
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner obj = new StockSpanner();
+ * int param_1 = obj.next(price);
+ */
