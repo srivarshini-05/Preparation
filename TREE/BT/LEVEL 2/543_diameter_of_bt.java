9090473587b9accf1bb96972184a7600efc1326b
Srivarshini_05 <165925681+srivarshini-05@users.noreply.github.com>
2026-01-17 14:17:55 +0530

    Create 543_diameter_of_bt.java

---
+++ TREE/BT/LEVEL 2/543_diameter_of_bt.java
@@ -0,0 +1,16 @@
+class Solution {
+    int ans=0;
+    public int diameterOfBinaryTree(TreeNode root) {
+        
+        dfs(root);
+        return ans;
+
+    }
+    public int dfs(TreeNode root){
+        if(root==null) return 0;
+        int l=dfs(root.left);
+        int r=dfs(root.right);
+        ans=Math.max(ans,l+r);
+        return 1+Math.max(l,r);
+    }
+}
