6f01bc24515d3c6cb9fdf0cfcff4524289cc69b5
Srivarshini_05 <165925681+srivarshini-05@users.noreply.github.com>
2026-03-23 18:06:08 +0530

    Create levelorder.java

---
+++ TREE/BT/TRAVERSAL LOOPS/levelorder.java
@@ -0,0 +1,21 @@
+public class LevelOrderTraversal {
+    public static void levelOrder(TreeNode root) {
+        if (root == null) return;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            TreeNode current = queue.poll();
+            System.out.print(current.val + " ");
+
+            if (current.left != null) {
+                queue.add(current.left);
+            }
+
+            if (current.right != null) {
+                queue.add(current.right);
+            }
+        }
+    }
+}
