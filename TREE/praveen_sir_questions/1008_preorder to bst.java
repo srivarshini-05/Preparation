aebdd23c89d11e374b229024972ddb9d2e990b72
Srivarshini_05 <165925681+srivarshini-05@users.noreply.github.com>
2026-04-05 11:05:58 +0530

    Create 1008_preorder to bst.java

---
+++ TREE/praveen_sir_questions/1008_preorder to bst.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode bstFromPreorder(int[] preorder) {
+        return helper(preorder,Integer.MAX_VALUE,new int[]{0});
+    }
+    public TreeNode helper(int[] arr,int b,int[] i){
+        if(i[0]==arr.length || arr[i[0]]>b) return null;
+        TreeNode root=new TreeNode(arr[i[0]++]);
+        root.left=helper(arr,root.val,i);
+        root.right=helper(arr,b,i);
+        return root;
+    }
+}
